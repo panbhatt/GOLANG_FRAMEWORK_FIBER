eee9815236029834bfaea71e1e33790270012139
Pankaj Bhatt <panbhatt@gmail.com>
2022-01-17 00:14:39 -0500

    Mount Subapp

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	fiber "github.com/gofiber/fiber/v2"
 	"net/http"
+	"time"
 )
 
 
@@ -13,7 +15,10 @@ func main() {
 			AppName : "Test App v1.0.1",
 		})
 
-		app.Static("/html" , "./public/html")
+		app.Static("/html" , "./public/html", fiber.Static {
+			Index : "index.html",
+			CacheDuration : 10 * time.Second,
+		})
 		app.Get("/", func(c *fiber.Ctx) error {
 				return c.SendString("HELLO WORLD")
 		})
@@ -28,7 +33,29 @@ func main() {
 			return fiber.NewError(http.StatusBadRequest, "Custom Error message")
 		})
 
+		// API Related Routes.
+		app.Use("/api/*", func (c *fiber.Ctx) error {
+			fmt.Println("MIddleware Invokced")
+			return c.Next()
+		})
+
+		app.Get("/api/v1", func(c *fiber.Ctx) error {
+			return c.SendString("Returning the APi Version v1")
+		})
+
+		// Mount SubAPp
+		app.Mount("subapp", GetSubApp())
+		Mount()
+
 
 		app.Listen(":3000")
 
+}
+
+func GetSubApp1() *fiber.App {
+	subAp := fiber.New()
+	subAp.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Sending it from sub app")
+	})
+	return subAp
 }
--- mountsubapp.go
+++ mountsubapp.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetSubApp() *fiber.App {
+	subAp := fiber.New()
+	subAp.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Sending it from sub app")
+	})
+	return subAp
+}
+
+func Mount(){
+	fmt.Println("IN Mount function")
+}
